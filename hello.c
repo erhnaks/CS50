f4afb6348b4fa006179abaa0ab26cd38e6fb02c6
erhnaks <erhnaks@hotmail.com>
2023-01-31 14:35:49 +0000

    Getting name in C

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+// #include <string>
 
 int main(void)
 {
-  printf("Hello, Ozan\n");
+    char name[20];
+    printf("Hello. What's your name?\n");
+    fgets(name,20,stdin);
+    printf("Hi there, %s", name);
+    return 0;
 }
