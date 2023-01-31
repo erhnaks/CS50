2b318ae3c7e013716ad5d12a5746d53ee9e5df8e
erhnaks <erhnaks@hotmail.com>
2023-01-31 15:20:26 +0000

    Conditionals in C

---
+++ conditionals.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+int x = 7;
+int y = 3;
+
+if (x > y){
+  printf(" x is greater than y\n");
+}
+else if (x < y){
+  printf(" x is not greater than y\n");
+}
+else{
+  printf(" x is equal to y\n");
+}
+}
