aabe7655b3a8f1653026e70ab7cace141e176b6a
erhnaks <erhnaks@hotmail.com>
2023-02-02 11:21:15 +0000

    loops in C

---
+++ loops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+  // this is while loop
+// int i = 0;
+
+// while (i<3)
+// {
+//   printf("meow\n");
+//   i++;
+// }
+
+// this is for loop
+for (int i = 0; i<10; i++){
+  printf("meow\n");
+}
+
+
+}
