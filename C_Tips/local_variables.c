0c2f6fea7c8341f16766f51e9596a227c238ab30
erhnaks <erhnaks@hotmail.com>
2023-02-13 10:37:56 +0000

    Global Variables

--- C_Tips/local_variables.c
+++ C_Tips/local_variables.c
@@ -12,3 +12,20 @@ int triple(x)
 
 // X is local to the function triple() No other function can refer to it, not  even main method.
 // result is loca to main!
+
+// GLOBAL VARIABLES --------------------------------
+
+// Global variables exist too. If a variable is declared outside of all functions, any function may refer to it.
+
+float global = 0.5050;
+
+int main (void)
+{
+  triple();
+  printf("%f\n", global);
+}
+
+void triple (void)
+{
+  global *= 3;
+}
